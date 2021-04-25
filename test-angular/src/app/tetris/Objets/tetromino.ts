2b0f8ca245c52122f245f0528c7c6e30cee2de82
Baptiste TRAUTMANN <baptistetrautmann@hotmail.fr>
2021-04-25 20:50:27 +0200

    implementation blocage déplacement hors grille

--- test-angular/src/app/tetris/Objets/tetromino.ts
+++ test-angular/src/app/tetris/Objets/tetromino.ts
@@ -1,124 +1,268 @@
+import { Grid } from "./grid";
 import { Square } from "./square";
+
 
 export abstract class Tetromino {
 
-  public centerPosX: number = 19;
-  public centerPosY: number = 10;
-  public direction: number = 1;
+  public centerPosX: number = 4;
+  public centerPosY: number = -1;
+
+  public blocks : Array<Square>;
+  public directions : Array<Array<Square>> = new Array();
 
   constructor(public color:string = "purple"){
-
+    this.blocks = this.directions[0];
   }
 
-  public abstract display(square_list: Array<Square>):void;
 
-  public abstract rotate(): void;
+  public display(square_list: Array<Square>){
+    square_list.filter(square => square.filled == false).map(square => square.color = "white");
+    this.blocks.forEach(
+      block => {
+        let square = square_list.find(square => square.height_position == (block.height_position + this.centerPosY) && square.width_position == (block.width_position + this.centerPosX) );
+        if(square != undefined){
+          square.color = this.color;
+        }
+      }
+    );
+  }
+
+  public rotate(grid: Grid){
+
+    let index :number = this.directions.findIndex( direction => this.blocks == direction);
+
+    if(index < this.directions.length-1){
+      this.blocks = this.directions[index+1];
+      if(!this.isLegal(grid,0)){
+        this.blocks = this.directions[index];
+      }
+    } else {
+      this.blocks = this.directions[0];
+      if(!this.isLegal(grid,0)){
+        this.blocks = this.directions[index];
+      }
+    }
+  }
+
+  public isLegal(grid: Grid, direction: number): boolean {
+    let resultat: boolean = true;
+    let posXOffset: number = 0;
+    let posYOffset: number = 0;
+    if(direction == 1){
+      posXOffset--;
+    } else if (direction == 2){
+      posYOffset++;
+    } else if (direction == 3){
+      posXOffset++;
+    }
+
+    this.getBlocks().forEach(block => {
+      if(this.centerPosY + block.height_position + posYOffset >= grid.height){
+        resultat= false;
+      }
+      if((this.centerPosX + block.width_position + posXOffset < 0) || (this.centerPosX + block.width_position + posXOffset >= grid.width)){
+        resultat= false;
+      }
+    });
+
+    return resultat;
+  }
+
+  public getBlocks(): Square[]{
+    return this.blocks;
+  }
+
+  public checkAndMoveDown(grid:Grid): void{
+    if(this.isLegal(grid, 2)){
+      this.centerPosY++;
+    }
+  }
+
+  public checkAndMoveLeft(grid:Grid): void{
+    if(this.isLegal(grid, 1)){
+      this.centerPosX--;
+    }
+  }
+
+  public checkAndMoveRight(grid:Grid): void{
+    if(this.isLegal(grid, 3)){
+      this.centerPosX++;
+    }
+  }
 
 }
 
 export class TetrominoI extends Tetromino {
 
-  public display(square_list: Array<Square>){
-    square_list.filter(square => square.filled == false).map(square => square.color = "white")
-    if(this.direction == 1){
-      square_list.forEach(square => this.direction1(square));
-    } else {
-      square_list.forEach(square => this.direction2(square));
-    }
-  }
+  centerPosY: number = -3;
 
-  public rotate(){
-    if(this.direction == 1){
-      this.direction++;
-    } else {
-      this.direction = 1;
-    }
-  }
+  private position1 = new Array<Square> (
+    new Square(2,0),
+    new Square(1,0),
+    new Square(0,0),
+    new Square(-1,0)
+  );
+  private position2 = new Array<Square> (
+    new Square(0,-1),
+    new Square(0,0),
+    new Square(0,1),
+    new Square(0,2)
+  );
 
-  private direction1(square:Square) {
-    if(square.width_position == this.centerPosX
-      && square.height_position <= this.centerPosY +2
-      && square.height_position >= this.centerPosY -1)
-    {
-      square.color = this.color;
-    }
-  }
+  directions = new Array<Array<Square>>(this.position1, this.position2)
+  blocks = this.position1;
 
-  private direction2(square:Square) {
-    if(square.width_position <= this.centerPosX +2
-      && square.width_position >= this.centerPosX -1
-      && square.height_position == this.centerPosY)
-    {
-      square.color = this.color;
-    }
-  }
 }
 
 export class TetrominoJ extends Tetromino {
 
-  public display(square_list: Array<Square>){
-    square_list.filter(square => square.filled == false).map(square => square.color = "white")
-    if(this.direction == 1){
-      square_list.forEach(square => this.direction1(square));
-    } else if(this.direction == 2) {
-      square_list.forEach(square => this.direction2(square));
-    } else if(this.direction == 3) {
-      square_list.forEach(square => this.direction3(square));
-    } else if(this.direction == 4) {
-      square_list.forEach(square => this.direction4(square));
-    }
-  }
+  private position1 = new Array<Square> (
+    new Square(-1,-1),
+    new Square(0,-1),
+    new Square(0,0),
+    new Square(0,+1)
+  );
+  private position2 = new Array<Square> (
+    new Square(-1,1),
+    new Square(1,0),
+    new Square(0,0),
+    new Square(-1,0)
+  );
+  private position3 = new Array<Square> (
+    new Square(0,-1),
+    new Square(0,0),
+    new Square(0,1),
+    new Square(1,1)
+  );
+  private position4 = new Array<Square> (
+    new Square(1,0),
+    new Square(0,0),
+    new Square(-1,0),
+    new Square(1,-1)
+  );
 
-  public rotate(){
-    if(this.direction < 4){
-      this.direction++;
-    } else {
-      this.direction = 1;
-    }
-  }
-
-  private direction1(square:Square) {
-    if((square.width_position <= this.centerPosX +1
-        && square.width_position >= this.centerPosX -1
-        && square.height_position == this.centerPosY)
-      || (square.width_position == this.centerPosX -1
-        && square.height_position == this.centerPosY -1))
-    {
-      square.color = this.color;
-    }
-  }
-
-  private direction2(square:Square) {
-    if((square.height_position <= this.centerPosY +1
-        && square.height_position >= this.centerPosY -1
-        && square.width_position == this.centerPosX)
-      || (square.width_position == this.centerPosX +1
-        && square.height_position == this.centerPosY -1))
-    {
-      square.color = this.color;
-    }
-  }
-
-  private direction3(square:Square) {
-    if((square.width_position <= this.centerPosX +1
-        && square.width_position >= this.centerPosX -1
-        && square.height_position == this.centerPosY)
-      || (square.width_position == this.centerPosX +1
-        && square.height_position == this.centerPosY +1))
-    {
-      square.color = this.color;
-    }
-  }
-
-  private direction4(square:Square) {
-    if((square.height_position <= this.centerPosY +1
-        && square.height_position >= this.centerPosY -1
-        && square.width_position == this.centerPosX)
-      || (square.width_position == this.centerPosX -1
-        && square.height_position == this.centerPosY +1))
-    {
-      square.color = this.color;
-    }
-  }
+  directions = new Array<Array<Square>>(this.position1, this.position2, this.position3, this.position4);
+  blocks = this.position1;
 }
 
+export class TetrominoL extends Tetromino {
+
+  private position1 = new Array<Square> (
+    new Square(0,-1),
+    new Square(0,0),
+    new Square(0,1),
+    new Square(-1,1)
+  );
+  private position2 = new Array<Square> (
+    new Square(1,0),
+    new Square(0,0),
+    new Square(-1,0),
+    new Square(1,1)
+  );
+  private position3 = new Array<Square> (
+    new Square(0,-1),
+    new Square(0,0),
+    new Square(0,1),
+    new Square(1,-1)
+  );
+  private position4 = new Array<Square> (
+    new Square(1,0),
+    new Square(0,0),
+    new Square(-1,0),
+    new Square(-1,-1)
+  );
+
+  directions = new Array<Array<Square>>(this.position1, this.position2, this.position3, this.position4);
+  blocks = this.position1;
+}
+
+export class TetrominoO extends Tetromino {
+
+  centerPosY: number = -2;
+
+  private position1 = new Array<Square> (
+    new Square(0,0),
+    new Square(1,0),
+    new Square(0,1),
+    new Square(1,1)
+  );
+
+  directions = new Array<Array<Square>>(this.position1);
+  blocks = this.position1;
+}
+
+export class TetrominoS extends Tetromino {
+
+  private position1 = new Array<Square> (
+    new Square(0,0),
+    new Square(0,-1),
+    new Square(-1,0),
+    new Square(-1,1)
+  );
+
+  private position2 = new Array<Square> (
+    new Square(0,1),
+    new Square(-1,1),
+    new Square(-1,0),
+    new Square(-2,0)
+  );
+
+  directions = new Array<Array<Square>>(this.position1,this.position2);
+  blocks = this.position1;
+}
+
+export class TetrominoT extends Tetromino {
+
+  private position1 = new Array<Square> (
+    new Square(0,0),
+    new Square(0,-1),
+    new Square(0,1),
+    new Square(-1,0)
+  );
+
+  private position2 = new Array<Square> (
+    new Square(-1,0),
+    new Square(0,0),
+    new Square(1,0),
+    new Square(0,1)
+  );
+
+  private position3 = new Array<Square> (
+    new Square(0,-1),
+    new Square(0,0),
+    new Square(1,0),
+    new Square(0,1)
+  );
+
+  private position4 = new Array<Square> (
+    new Square(-1,0),
+    new Square(0,0),
+    new Square(1,0),
+    new Square(0,-1)
+  );
+
+  directions = new Array<Array<Square>>(this.position1,this.position2, this.position3,this.position4);
+  blocks = this.position1;
+}
+
+export class TetrominoZ extends Tetromino {
+
+  centerPosY: number = -2;
+
+  private position1 = new Array<Square> (
+    new Square(0,0),
+    new Square(0,-1),
+    new Square(1,0),
+    new Square(1,1)
+  );
+
+  private position2 = new Array<Square> (
+    new Square(0,0),
+    new Square(-1,0),
+    new Square(0,-1),
+    new Square(1,-1)
+  );
+
+  directions = new Array<Array<Square>>(this.position1,this.position2);
+  blocks = this.position1;
+}
